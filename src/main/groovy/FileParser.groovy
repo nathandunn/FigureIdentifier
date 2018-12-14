e50814c3459a3a48d6593302dce608e2209fa36a
Nathan Dunn <ndunn@me.com>
2018-12-14 14:48:25 -0800

    identified images versus non-image files

--- src/main/groovy/FileParser.groovy
+++ src/main/groovy/FileParser.groovy
@@ -1,32 +1,67 @@
+import groovy.io.FileType
 
 
+// maybe we can use the google drive?
+def inputImagePath = new File("/Users/nathandunn/INCA/ToAnalyze/single_files/")
 
-def f = new File('../../../resources/figshare_formatted_image_files.tsv')
-println f.absolutePath
-println f.exists()
+
+def singleImagePath = new File("paththere")
+def singleNonImagePath = new File("paththere")
+
+def formattedCodes = new File('../../../resources/figshare_formatted_image_files.tsv')
+println formattedCodes.absolutePath
+println formattedCodes.exists()
 
 def imageFiles = []
 def nonImageFiles = []
+def filesToCopy = []
 
-def imagePattern=["I"]
-def noImagePattern=["D"]
+def imagePattern=["I","MI","BI"]
+def imageFileSuffixes=["tif","png","pdf","jpg","jpeg"]
+def noImagePattern=["C","LC","BC","D","M","O","NN"]
 
-f.text.readLines().eachWithIndex { line,index ->
+formattedCodes.text.readLines().eachWithIndex { line,index ->
     def splitLine = line.split("\t")
-//    println index + "(1)" + splitLine[0] + "(2)" + splitLine[1]
-//    if(splitLine.length>2){
-//        println index + "(3)" + splitLine[2]
-//    }
-    if(imagePattern.contains(splitLine[1])){
-        imageFiles.add(splitLine[0])
+
+    def directory = splitLine[0]
+    def code = splitLine[1]
+    def note = splitLine.length>2 ? splitLine[2] : ""
+    if(imagePattern.contains(code) &&  !note?.toLowerCase()?.contains("multi")){
+        imageFiles.add(directory)
     }
     else{
-        imageFiles.add(splitLine[0])
+        nonImageFiles.add(directory)
     }
 }
 
-// then copy ONLY the files
 
+println "Image files: " +imageFiles.size()
+println "Non image files: " +nonImageFiles.size()
+
+// list contents of directories for each
+imageFiles.eachWithIndex { String entry, int i ->
+    String fileInput = "${inputImagePath.absolutePath}/${entry}/"
+//    println "file input ${fileInput}"
+    def dir  = new File(fileInput)
+//    println "dir ${dir.absolutePath}"
+    def filesToCopyHere = []
+    dir.eachFileRecurse (FileType.FILES) { File file ->
+        def fileSplit = file.name.toLowerCase().split("\\.")
+        def suffix = fileSplit[fileSplit.length-1]
+        if(imageFileSuffixes.contains(suffix)){
+            filesToCopyHere << file
+        }
+    }
+    if(filesToCopyHere.size()>1){
+        println "too many to copy for ${entry} -> ${filesToCopyHere}"
+    }
+    filesToCopy << filesToCopyHere
+}
+
+println filesToCopy.size()
+println filesToCopy
+
+// then copy ONLY the files
 
 // then score the separate as 100%
 
